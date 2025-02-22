81bdda92a9813bd2c3204af5863c0e2a757b7b3c
Livens <dangkhoah77@gmail.com>
2025-02-22 22:46:50 +0700

    [feat] remove unnecessary attributes and functions

--- Level.py
+++ Level.py
@@ -5,26 +5,19 @@ from copy import deepcopy
 class Level:
     """The class that represents the level of the game."""
 
-    matrix = []
-    """The matrix of the level."""
-    matrixHistories = []
-    """The storage to older versions of the level matrix."""
-
-    playerPosition = []
-    """The position of the player."""
-
-    boxes = []
-    """The positions of the boxes."""
-    switches = []
-    """The positions of the switches."""
-
     def __init__(self, levelNumber: int):
         """Initialize the level and getting the player position, boxes, and switches.
 
         ### Parameters
         @levelNumber: The level number to be loaded.
         """
+        self.matrix = []
+        self.matrixSize = [0, 0]
+        self.playerPosition = []
+        self.boxes = {}
+        self.switches = []
         # Create level
+        weights = []
         with open(
             os.path.dirname(os.path.abspath(__file__))
             + "/levels/"
@@ -32,51 +25,40 @@ class Level:
             + str(levelNumber),
             "r",
         ) as f:
+            # Get the weights of the boxes
+            weights = f.readline().split()
+            # Get the size while parsing the matrix
+            largestRowLength = 0
+            # Iterate all rows
             for row in f.read().splitlines():
                 self.matrix.append(list(row))
+                # Update the largest row length
+                largestRowLength = (
+                    len(self.matrix[self.matrixSize[1]])
+                    if len(self.matrix[self.matrixSize[1]]) > largestRowLength
+                    else largestRowLength
+                )
+                self.matrixSize[1] += 1
+            # Set the row length of the matrix
+            self.matrixSize[0] = largestRowLength
 
-        # Get player position
+        # Get player position, boxes and switches
         for i in range(0, len(self.matrix)):
             # Iterate all columns
             for k in range(0, len(self.matrix[i]) - 1):
                 if self.matrix[i][k] == "@" or self.matrix[i][k] == "+":
                     self.playerPosition = [k, i]
-                    break
-
-        # Get boxes and switches
-        for i in range(0, len(self.matrix)):
-            # Iterate all columns
-            for k in range(0, len(self.matrix[i]) - 1):
-                if self.matrix[i][k] == "$":
-                    self.boxes.append([k, i])
+                elif self.matrix[i][k] == "$":
+                    self.boxes.update({(k, i): int(weights[len(self.boxes)])})
                 elif self.matrix[i][k] == ".":
                     self.switches.append([k, i])
                 elif self.matrix[i][k] == "*":
-                    self.boxes.append([k, i])
+                    self.boxes.update({(k, i): int(weights[len(self.boxes)])})
                     self.switches.append([k, i])
-
-    def __del__(self):
-        """Clear the level's data when the object is deleted."""
-        self.matrix.clear()
-        self.matrixHistories.clear()
-        self.playerPosition.clear()
-        self.boxes.clear()
-        self.switches.clear()
 
     def getMatrix(self):
         """Return the current matrix of the level."""
         return self.matrix
-
-    def getLastMatrix(self):
-        """Return the last matrix from the history and set it as the current matrix.
-        If no history is available, return the current matrix.
-        """
-        if len(self.matrixHistories) > 0:
-            lastMatrix = self.matrixHistories.pop()
-            self.matrix = lastMatrix
-            return lastMatrix
-        else:
-            return self.matrix
 
     def getPlayerPosition(self):
         """Return the current position of the player."""
@@ -92,20 +74,10 @@ class Level:
 
     def getSize(self):
         """Return the size of the matrix."""
-        largestRowLength = 0
-        # Iterate all rows
-        for i in range(0, len(self.matrix)):
-            # Get the length of each row
-            rowLength = len(self.matrix[i])
-            # Update the largest row length if the current row length is larger
-            if rowLength > largestRowLength:
-                largestRowLength = rowLength
-        return [largestRowLength, len(self.matrix)]
+        return self.matrixSize
 
-    def addToHistory(self, matrix: list):
-        """Store the provided matrix to the history list.
-
-        ### Parameters
-        @matrix: The matrix to be stored in the history list.
-        """
-        self.matrixHistories.append(deepcopy(matrix))
+    def printMatrix(self):
+        """Print the matrix of the level."""
+        for row in self.matrix:
+            print("".join(row))
+        print("\n")
