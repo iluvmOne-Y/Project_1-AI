64ef393a4fb3be17cf353e8bd2ead012257036ac
Livens <dangkhoah77@gmail.com>
2025-03-03 02:55:49 +0700

    Refactor algorithm selection to integrate additional search algorithms

--- Source/main.py
+++ Source/main.py
@@ -1,9 +1,23 @@
+import _TYPES as _TYPES
+
+# Intergated modules
+import pathlib as pathlib
+
 # Custom modules
 import Controller.InterfaceController as InterfaceController
 import Controller.LevelController as LevelController
 
+# Custom modules
+from Algorithms.BFS import BFS
+from Algorithms.DFS import DFS
+from Algorithms.UCS import UCS
+from Algorithms.AStar import AStar
+from Algorithms.Dijkstra import Dijkstra
+from Algorithms.GreedyBFS import GreedyBFS
+
+# Get the algorithms
+algorithms: list = [BFS, DFS, UCS, AStar, Dijkstra, GreedyBFS]
 
 while True:
-    level, algorithm = InterfaceController.DrawSelectionMenu()
-    print(level, algorithm)
+    level, algorithm = InterfaceController.DrawSelectionMenu(algorithms)
     LevelController.InitLevel(level, algorithm)
